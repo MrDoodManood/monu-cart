dcdfcbcf9ec73d45fc84b359e468fff252d7a0fb
MrDoodManood <makancary@gmail.com>
2026-09-01 22:06:34 +0400

    Update products.js

--- products.js
+++ products.js
@@ -11,7 +11,7 @@ const PRODUCTS = [
         {
         name: "Beef Milk",
         price: "$29.99",
-        description: "Funniest mug in town!",
+        description: "Why drink milk if you can drink beef? Or maybe both!",
         category: "Consumable Items",
         image: "./products/product2.png"
     }
